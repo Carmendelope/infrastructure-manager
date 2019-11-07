cc91386fd3ed962ca612edcb26a23151e189885f
Gaizka Díaz <gdiaz@nalej.com>
2019-11-07 15:43:41 +0100

    NP-2159 prepare repo for open source

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # infrastructure-manager
 
 This repository contains the infrastructure manager in charge of the operations related to adding and removing infrastructure.
-The component will leverage the Installer, Provisioner and System Model to orchestrate the business logic behing infrastructure operations.
 
-## Launching the manager
+## Getting Started
+
+The component will leverage the `installer`, `provisioner` and `system-model` to orchestrate the business logic behind infrastructure operations.
+
+### Prerequisites
+
+Detail any component that has to be installed to run this component.
+
+* system-model
+* installer
+* provisioner
+* nalej-nus
+
+### Build and compile
+
+In order to build and compile this repository use the provided Makefile:
 
 ```
-$ ./bin/infrastructure-manager run --debug --consoleLogging
+make all
 ```
 
-# Integration tests
+This operation generates the binaries for this repo, download dependencies,
+run existing tests and generate ready-to-deploy Kubernetes files.
 
-The following table contains the variables that activate the integration tests
+### Run tests
+
+Tests are executed using Ginkgo. To run all the available tests:
+
+```
+make test
+```
+
+### Integration tests
+
+The following table contains the variables that activate the integration tests:
 
 | Variable  | Example Value | Description |
 | ------------- | ------------- |------------- |
@@ -19,4 +44,34 @@ The following table contains the variables that activate the integration tests
 | IT_SM_ADDRESS  | localhost:8800 | System Model Address |
 | IT_PROVISIONER_ADDRESS  | localhost:8930 | Provisioner Address |
 | IT_INSTALLER_ADDRESS  | localhost:8900 | Installer Address |
-| IT_K8S_KUBECONFIG | /Users/daniel/.kube/config| KubeConfig for the minikube credentials |
+| IT_K8S_KUBECONFIG | /Users/pedro/.kube/config| KubeConfig for the minikube credentials |
+
+### Update dependencies
+
+Dependencies are managed using Godep. For an automatic dependencies download use:
+
+```
+make dep
+```
+
+In order to have all dependencies up-to-date run:
+
+```
+dep ensure -update -v
+```
+
+## Contributing
+
+Please read [contributing.md](contributing.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/nalej/infrastructure-manager/tags). 
+
+## Authors
+
+See also the list of [contributors](https://github.com/nalej/infrastructure-manager/contributors) who participated in this project.
+
+## License
+This project is licensed under the Apache 2.0 License - see the [LICENSE-2.0.txt](LICENSE-2.0.txt) file for details.
--- internal/pkg/server/discovery/k8s/discovery.go
+++ internal/pkg/server/discovery/k8s/discovery.go
@@ -1,5 +1,17 @@
 /*
- * Copyright (C) 2018 Nalej - All Rights Reserved
+ * Copyright 2019 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // TODO Refactor this code an move it outside of the infrastructure-manager
@@ -18,9 +30,9 @@ import (
 
 type DiscoveryHelper struct {
 	KubeConfigPath string
-	Client * kubernetes.Clientset
-	ClusterName string
-	Server string
+	Client         *kubernetes.Clientset
+	ClusterName    string
+	Server         string
 }
 
 func NewDiscoveryHelper(kubeConfigPath string) *DiscoveryHelper {
@@ -30,7 +42,7 @@ func NewDiscoveryHelper(kubeConfigPath string) *DiscoveryHelper {
 }
 
 // Validate checks that the KubeConfig file is valid and preloads basic data of the target cluster.
-func (dh * DiscoveryHelper) Validate() derrors.Error {
+func (dh *DiscoveryHelper) Validate() derrors.Error {
 	cclr := clientcmd.ClientConfigLoadingRules{ExplicitPath: dh.KubeConfigPath}
 	cfg, err := cclr.Load()
 	if err != nil {
@@ -51,7 +63,7 @@ func (dh * DiscoveryHelper) Validate() derrors.Error {
 }
 
 // Connect processes the configuration validating it first, and establishes the client channels with the cluster.
-func (dh * DiscoveryHelper) Connect() derrors.Error {
+func (dh *DiscoveryHelper) Connect() derrors.Error {
 	vErr := dh.Validate()
 	if vErr != nil {
 		return vErr
@@ -67,14 +79,14 @@ func (dh * DiscoveryHelper) Connect() derrors.Error {
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Error().Err(err).Msg("error using configuration to build k8s clientset")
-		return derrors.AsError(err,"error using configuration to build k8s clientset")
+		return derrors.AsError(err, "error using configuration to build k8s clientset")
 	}
 
 	dh.Client = clientset
 	return nil
 }
 
-func (dh * DiscoveryHelper) Discover() (* entities.Cluster, derrors.Error) {
+func (dh *DiscoveryHelper) Discover() (*entities.Cluster, derrors.Error) {
 	sv, err := dh.Client.Discovery().ServerVersion()
 	if err != nil {
 		return nil, derrors.AsError(err, "cannot read version")
@@ -98,17 +110,17 @@ func (dh * DiscoveryHelper) Discover() (* entities.Cluster, derrors.Error) {
 	}
 
 	/*
-	host, _, err := net.SplitHostPort(u.Host)
-	if err != nil{
-		return nil, derrors.AsError(err, "cannot extract target cluster hostname")
-	}
+		host, _, err := net.SplitHostPort(u.Host)
+		if err != nil{
+			return nil, derrors.AsError(err, "cannot extract target cluster hostname")
+		}
 	*/
 
 	return &entities.Cluster{
-		KubernetesVersion: sv.String(),
-		Name: dh.ClusterName,
-		Description: "Autodiscovered cluster",
+		KubernetesVersion:    sv.String(),
+		Name:                 dh.ClusterName,
+		Description:          "Autodiscovered cluster",
 		ControlPlaneHostname: u.Hostname(),
-		Nodes:             nodes,
+		Nodes:                nodes,
 	}, nil
 }
